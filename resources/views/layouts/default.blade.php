d10ac0c92d8c7c2c13b6d78c9a154cedae52aab9
ylphp <1083419457@qq.com>
2020-07-13 12:52:00 +0000

    创建会话

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <title>@yield('title', 'Weibo App') - 叶亮的专属网站</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
